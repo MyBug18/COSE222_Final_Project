cae869d74b8ac870d13ee8de5811440d17095c3e
MyBug18 <mskimkim0318@gmail.com>
2019-11-20 13:29:49 +0900

    Added pipelined (Test required)

--- Pipelined/MIPS_CPU/mips.v
+++ Pipelined/MIPS_CPU/mips.v
@@ -135,14 +135,14 @@ module ID_EX(input clk,
 				 input [2:0] alucontrol,
 				 input [31:0] pc, 
 				 input [31:0] rd1, rd2,
-				 input [31:0] imm_val, shifted16_val,
-				 input [5:0] inst20_16, inst15_11,
+				 input [31:0] imm_val,
+				 input [4:0] inst20_16, inst15_11,
 				 output reg memtoreg_out, memwrite_out, branch_out, alusrc_out, regdst_out, regwrite_out, jump_out, 
 				 output reg [2:0] alucontrol_out, 
 				 output reg [31:0] pc_out, 
 				 output reg [31:0] rd1_out, rd2_out, 
-				 output reg [31:0] imm_val_out, shifted16_val_out, 
-				 output reg [5:0] inst20_16_out, inst15_11_out);
+				 output reg [31:0] imm_val_out,
+				 output reg [4:0] inst20_16_out, inst15_11_out);
 				 
   always @(posedge clk)
     begin
@@ -158,12 +158,54 @@ module ID_EX(input clk,
 		rd1_out <= #`mydelay rd1;
 		rd2_out <= #`mydelay rd2;
 		imm_val_out <= #`mydelay imm_val;
-		shifted16_val_out <= #`mydelay shifted16_val;
 		inst20_16_out <= #`mydelay inst20_16;
 		inst15_11_out <= #`mydelay inst15_11;
 	 end
 endmodule
-				 
+			
+module EX_MEM(input clk, 
+				  input memtoreg, memwrite, branch, regwrite, jump,
+				  input [31:0] pc_for_j, pc_for_beq, alu_result,
+				  input is_zero,
+				  input [31:0] writedata,
+				  input [4:0] regdst_addr,
+				  output reg memtoreg_out, memwrite_out, branch_out, regwrite_out, jump_out,
+				  output reg [31:0] pc_for_j_out, pc_for_beq_out, alu_result_out,
+				  output reg is_zero_out, 
+				  output reg [31:0] writedata_out, 
+				  output reg [4:0] regdst_addr_out);
+				  
+  always @(posedge clk)
+    begin
+		memtoreg_out <= #`mydelay memtoreg;
+		memwrite_out <= #`mydelay memwrite;
+		branch_out <= #`mydelay branch;
+		regwrite_out <= #`mydelay regwrite;
+		jump_out <= #`mydelay jump;
+		is_zero_out <= #`mydelay is_zero;
+		alu_result_out <= #`mydelay alu_result;
+		writedata_out <= #`mydelay writedata;
+		regdst_addr_out <= #`mydelay regdst_addr;
+	 end
+endmodule
+		
+module MEM_WB (input clk, 
+					input regwrite, memtoreg, 
+					input [31:0] readdata, aluresult, 
+					input [4:0] regdst_addr,
+					output reg regwrite_out, memtoreg_out, 
+					output reg [31:0] readdata_out, aluresult_out,
+					output reg [4:0] regdst_addr_out);
+  always @(posedge clk)			
+	 begin
+	   regwrite_out <= #`mydelay regwrite;
+		memtoreg_out <= #`mydelay memtoreg;
+		regdst_addr_out <= #`mydelay regdst_addr;
+		readdata_out <= #`mydelay readdata;
+		memtoreg_out <= #`mydelay memtoreg;
+	 end
+endmodule
+		
 module datapath(input         clk, reset,
                 output        final_zero,
 					 output        final_memwrite,
@@ -185,12 +227,22 @@ module datapath(input         clk, reset,
   
   wire signext, shiftl16, memtoreg, memwrite, branch, alusrc, regdst, regwrite,jump;
   wire memtoreg_after_ID_EX, memwrite_after_ID_EX, branch_after_ID_EX, alusrc_after_ID_EX, regdst_after_ID_EX, regwrite_after_ID_EX,jump_after_ID_EX;
-  wire [2:0] alucontrol;
+  wire [2:0] alucontrol, alucontrol_after_ID_EX;
   
   wire [31:0] imm_val_after_ID_EX, shifted16_val_after_ID_EX, inst20_16_after_ID_EX, inst15_11_after_ID_EX;
+  
+  wire memtoreg_after_EX_MEM, memwrite_after_EX_MEM, branch_after_EX_MEM, regwrite_after_EX_MEM,jump_after_EX_MEM;
+  
+  wire [31:0] pc_for_j, pc_for_branch, writedata_after_ID_EX;
+  
+  wire is_zero;
+  
+  wire [4:0] regdst_addr, regdst_addr_after_MEM_WB;
+  
+  wire memtoreg_after_MEM_WB, regwrite_after_MEM_WB, aluresult_after_MEM_WB, readdata_after_MEM_WB;
 
   adder pcadd1(
-    .a (pc),
+    .a (final_pc),
     .b (32'b100),
     .y (pcplus4));
   
@@ -218,10 +270,10 @@ module datapath(input         clk, reset,
   // register file logic
   regfile rf(
     .clk     (clk),
-    .we      (regwrite_after_ID_EX),
+    .we      (regwrite_after_MEM_WB),
     .ra1     (instr_after_IF_ID[25:21]),
     .ra2     (instr_after_IF_ID[20:16]),
-    .wa      (writereg),
+    .wa      (regdst_addr_after_MEM_WB),
     .wd      (result),
     .rd1     (rd1),
     .rd2     (rd2));
@@ -245,11 +297,11 @@ module datapath(input         clk, reset,
   .regdst (regdst), 
   .regwrite (regwrite), 
   .jump (jump),
+  .alucontrol (alucontrol),
   .pc (pc_after_IF_ID), 
   .rd1 (rd1),
   .rd2 (rd2),
-  .imm_val (signimm),
-  .shifted16_val (shiftedimm),
+  .imm_val (shiftedimm),
   .inst20_16 (instr_after_IF_ID[20:16]),
   .inst15_11 (instr_after_IF_ID[15:11]),
   .memtoreg_out (memtoreg_after_ID_EX),
@@ -259,69 +311,105 @@ module datapath(input         clk, reset,
   .regdst_out (regdst_after_ID_EX),
   .regwrite_out (regwrite_after_ID_EX),
   .jump_out (jump_after_ID_EX),
+  .alucontrol_out (alucontrol_after_ID_EX),
   .pc_out (pc_after_ID_EX),
   .rd1_out (rd1_after_ID_EX),
   .rd2_out (rd2_after_ID_EX),
   .imm_val_out (imm_val_after_ID_EX),
-  .shifted16_val_out (shifted16_val_after_ID_EX),
   .inst20_16_out (inst20_16_after_ID_EX),
   .inst15_11_out (inst15_11_after_ID_EX));
   
-  
-  
-  // next PC logic
-  flopr #(32) pcreg(
-    .clk   (clk),
-    .reset (reset),
-    .d     (pcnext),
-    .q     (pc));
+  mux2 #(5) wrmux(
+    .d0  (inst20_16_after_ID_EX),
+    .d1  (inst15_11_after_ID_EX),
+    .s   (regdst_after_ID_EX),
+    .y   (writereg));
+
+  alu alu(
+    .a       (rd1_after_ID_EX),
+    .b       (srcb),
+    .alucont (alucontrol_after_ID_EX),
+    .result  (aluout),
+    .zero    (zero));	 
 
   sl2 immsh(
     .a (imm_val_after_ID_EX),
     .y (signimmsh));
-				 
+	 
   adder pcadd2(
     .a (pc_after_ID_EX),
     .b (signimmsh),
     .y (pcbranch));
 
+  EX_MEM ex_mem (
+    .clk (clk), 
+	 .memtoreg (memtoreg_after_ID_EX), 
+	 .memwrite (memwrite_after_ID_EX), 
+	 .branch (branch_after_ID_EX), 
+	 .regwrite (regwrite_after_ID_EX), 
+	 .jump (jump_after_ID_EX), 
+	 .pc_for_j ({pc_after_ID_EX[31:28], instr[25:0], 2'b00}), 
+	 .pc_for_beq (pcbranch), 
+	 .alu_result (aluout),
+	 .is_zero (zero), 
+	 .writedata (rd2_after_ID_EX), 
+	 .regdst_addr (writereg),
+	 .memtoreg_out (memtoreg_after_EX_MEM), 
+	 .memwrite_out (final_memwrite), 
+	 .branch_out (branch_after_EX_MEM), 
+	 .regwrite_out (regwrite_after_EX_MEM), 
+	 .jump_out (jump_after_EX_MEM), 
+	 .pc_for_j_out (pc_for_j), 
+	 .pc_for_beq_out (pc_for_branch), 
+	 .alu_result_out (final_aluout),
+	 .is_zero_out (is_zero), 
+	 .writedata_out (final_writedata), 
+	 .regdst_addr_out (regdst_addr)
+	 );
+  flopr #(32) pcreg(
+    .clk   (clk),
+    .reset (reset),
+    .d     (pcnext),
+    .q     (final_pc));
+
   mux2 #(32) pcbrmux(
-    .d0  (pc_after_ID_EX),
-    .d1  (pcbranch),
-    .s   (pcsrc),
+    .d0  (pcplus4),
+    .d1  (pc_for_branch),
+    .s   (branch_after_EX_MEM & is_zero),
     .y   (pcnextbr));
 
   mux2 #(32) pcmux(
     .d0   (pcnextbr),
-    .d1   ({pc_after_ID_EX[31:28], instr[25:0], 2'b00}),
-    .s    (jump_after_ID_EX),
+    .d1   (pc_for_j),
+    .s    (jump_after_EX_MEM),
     .y    (pcnext));
 
 
-  mux2 #(5) wrmux(
-    .d0  (inst20_16_after_ID_EX[20:16]),
-    .d1  (inst15_11_after_ID_EX[15:11]),
-    .s   (regdst_after_ID_EX),
-    .y   (writereg));
 
   mux2 #(32) resmux(
-    .d0 (aluout),
-    .d1 (readdata),
-    .s  (memtoreg_after_ID_EX),
+    .d0 (aluresult_after_MEM_WB),
+    .d1 (readdata_after_MEM_WB),
+    .s  (memtoreg_after_MEM_WB),
     .y  (result));
 
   // ALU logic
   mux2 #(32) srcbmux(
     .d0 (rd2_after_ID_EX),
-    .d1 (shifted16_val_after_ID_EX),
+    .d1 (imm_val_after_ID_EX),
     .s  (alusrc_after_ID_EX),
     .y  (srcb));
-
-  alu alu(
-    .a       (rd1_after_ID_EX),
-    .b       (srcb),
-    .alucont (alucontrol),
-    .result  (aluout),
-    .zero    (zero));
-    
+	 
+  MEM_WB mem_wb(
+  .clk (clk), 
+  .regwrite (regwrite_after_EX_MEM),
+  .memtoreg (memtoreg_after_EX_MEM),
+  .regdst_addr (regdst_addr),
+  .readdata (readdata),
+  .aluresult(final_aluout),
+  .regwrite_out (regwrite_after_MEM_WB),
+  .memtoreg_out (memtoreg_after_MEM_WB),
+  .regdst_addr_out (regdst_addr_after_MEM_WB),
+  .readdata_out (readdata_after_MEM_WB),
+  .aluresult_out(aluresult_after_MEM_WB),
+  );
 endmodule
